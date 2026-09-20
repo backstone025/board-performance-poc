44496bb188fe320665a8f7e5a55bef8371929a7d
backstone025 <112708588+backstone025@users.noreply.github.com>
2026-09-20 17:43:31 +0900

    refactor: 로컬 캐시(Caffeine)에서 원격 캐시(Redis)로 캐시 설정 클래스 수정

--- src/main/java/com/backstone/simple_board/global/config/CacheConfig.java
+++ src/main/java/com/backstone/simple_board/global/config/CacheConfig.java
@@ -1,29 +1,35 @@
 package com.backstone.simple_board.global.config;
 
-import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
 
     @Bean
-    public CacheManager cacheManager() {
-        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
 
-        // Caffeine 캐시 기본 세부 설정
-        cacheManager.setCaffeine(Caffeine.newBuilder()
-                .initialCapacity(10000)                         // 초기 캐시 공간(10,000)
-                .maximumSize(100000)                            // 최대 캐시 저장 개수(100,000)
-                .expireAfterWrite(1, TimeUnit.HOURS)    // write 후 1시간 뒤 만료(Test 실행 약 21분 동안 유지)
-                .recordStats());                                // Prometheus 지표 수집을 위한 캐시 통계 활성화
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofHours(1))  // 캐시 유효 시간 1시간으로 설정
+                .serializeKeysWith(             // Redis Key 직렬화(String)
+                        RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.string())
+                )
+                .serializeValuesWith(           // Redis Value 직렬화(String)
+                        RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.json())
+                );
 
-        return cacheManager;
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(config)
+                .build();
     }
 }
